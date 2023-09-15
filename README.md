cb7af349dc83898bf75ef1e15784703c7e0fb2ee
Adalto Picotti Junior <adalto@principia.net>
2023-09-14 22:21:01 -0300

    Projeto finalizado.

---
+++ README.md
@@ -0,0 +1,13 @@
+## Upload.ai
+
+### Projeto desenvolvido na NLW AI com a Rocketseat
+
+Upload AI, é uma ferramenta de upload de vídeos para o Youtube que cria automaticamente títulos, descrições e materiais para vídeo.
+
+[![OpenAI](https://img.shields.io/badge/Vite-FF9A00?style=for-the-badge&logo=openai&logoColor=fff)](https://openai.com/)
+[![Vite](https://img.shields.io/badge/Vite-FF9A00?style=for-the-badge&logo=vite&logoColor=fff)](https://vitejs.dev)
+[![ReactJS](https://img.shields.io/badge/React.JS-FF9A00?style=for-the-badge&logo=react&logoColor=fff)](https://react.dev)
+[![TypeScript](https://img.shields.io/badge/TypeScript-FF9A00?style=for-the-badge&logo=typescript&logoColor=fff)](https://www.typescriptlang.org)
+[![TypeScript](https://img.shields.io/badge/TailwindCSS-FF9A00?style=for-the-badge&logo=tailwindcss&logoColor=fff)](https://tailwindcss.com)
+
+![Screenshot](Screenshot.png)
--- upload-ai-web/src/components/video-input-form.tsx
+++ upload-ai-web/src/components/video-input-form.tsx
@@ -4,10 +4,28 @@ import { Button } from './ui/button'
 import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
 import { Separator } from './ui/separator'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from 'react'
+import { getFFmpeg } from '@/lib/ffmpeg'
+import { fetchFile } from '@ffmpeg/util'
+import { api } from '@/lib/axios'
 
-export function VideoInputForm() {
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
+const statusMessages = {
+  converting: 'Convertendo...',
+  generating: 'Transcrevendo...',
+  uploading: 'Carregando...',
+  success: 'Sucesso!'
+}
+
+interface VideoInputFormProps {
+  onVideoUploaded: (id: string) => void
+}
+
+export function VideoInputForm(props: VideoInputFormProps) {
   const [videoFile, setVideoFile] = useState<File | null>(null)
+  const [status, setStatus] = useState<Status>('waiting')
+  const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget
@@ -21,14 +39,99 @@ export function VideoInputForm() {
     setVideoFile(selectedFile)
   }
 
+  async function convertVideoToAudio(video: File) {
+    console.log('Convert started.')
+
+    const ffmpeg = await getFFmpeg()
+
+    await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+
+    // ffmpeg.on('log', log=> {
+    //   console.log(log)
+    // })
+
+    ffmpeg.on('progress', progress => {
+      console.log('Convert progress: ' + Math.round(progress.progress * 100))
+    })
+
+    await ffmpeg.exec([
+      '-i',
+      'input.mp4',
+      '-map',
+      '0:a',
+      '-b:a',
+      '20k',
+      '-acodec',
+      'libmp3lame',
+      'output.mp3'
+    ])
+
+    const data = await ffmpeg.readFile('output.mp3')
+
+    const audioFileBlob = new Blob([data], { type: 'audio/mpeg' })
+    const audioFile = new File([audioFileBlob], 'audio.mp3', {
+      type: 'audio/mpeg'
+    })
+
+    console.log('Convert finished.')
+
+    return audioFile
+  }
+
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const prompt = promptInputRef.current?.value
+
+    if (!videoFile) {
+      return
+    }
+
+    setStatus('converting')
+
+    // converter o vídeo em áudio
+    const audioFile = await convertVideoToAudio(videoFile)
+
+    const data = new FormData()
+    data.append('file', audioFile)
+
+    setStatus('uploading')
+
+    const response = await api.post('/videos', data)
+
+    const videoId = response.data.video.id
+
+    setStatus('generating')
+
+    await api.post(`/videos/${videoId}/transcription`, {
+      prompt
+    })
+
+    setStatus('success')
+
+    props.onVideoUploaded(videoId)
+  }
+
+  const previewURL = useMemo(() => {
+    if (!videoFile) {
+      return null
+    }
+
+    return URL.createObjectURL(videoFile)
+  }, [videoFile])
+
   return (
-    <form className="space-y-6">
+    <form onSubmit={handleUploadVideo} className="space-y-6">
       <label
         htmlFor="video"
-        className="border flex flex-col gap-2 items-center justify-center rounded-md aspect-video cursor-pointer border-dashed text-sm text-muted-foreground hover:bg-primary/5"
+        className="relative border flex flex-col gap-2 items-center justify-center rounded-md aspect-video cursor-pointer border-dashed text-sm text-muted-foreground hover:bg-primary/5"
       >
-        {videoFile ? (
-          'oi'
+        {previewURL ? (
+          <video
+            src={previewURL}
+            controls={false}
+            className="pointer-events-none absolute inset-0"
+          />
         ) : (
           <>
             <FileVideo className="w-4 h-4" />
@@ -49,14 +152,28 @@ export function VideoInputForm() {
       <div className="space-y-2">
         <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
         <Textarea
+          ref={promptInputRef}
+          disabled={status !== 'waiting'}
           id="transcription_prompt"
           className="h-20 leading-relaxed resize-none"
           placeholder="Inclua palavras-chave mencionadas no vídeo separadas por virgula ( , )"
         />
       </div>
 
-      <Button type="submit" className="w-full">
-        Carregar vídeo <Upload className="w-4 h-4 ml-2" />
+      <Button
+        data-success={status === 'success'}
+        disabled={status !== 'waiting'}
+        type="submit"
+        className="w-full data-[success=true]:bg-emerald-500 "
+      >
+        {status === 'waiting' ? (
+          <>
+            Carregar vídeo
+            <Upload className="w-4 h-4 ml-2" />
+          </>
+        ) : (
+          statusMessages[status]
+        )}
       </Button>
     </form>
   )
